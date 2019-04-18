3bbfce2b4ca3122996fb1f1b8b55a52147e0b44a
王旭东 <wangxudong@iwubida.com>
2019-04-18 11:11:15 +0800

    添加rabbitMQ

---
+++ springboot-rabbitMQ/src/main/java/com/wxd/RibbitMQApplication.java
@@ -0,0 +1,16 @@
+package com.wxd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author :wxd
+ * @Description:
+ * @Date: Created in 17:32 2019/4/15
+ */
+@SpringBootApplication
+public class RibbitMQApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RibbitMQApplication.class,args);
+    }
+}
---
+++ springboot-rabbitMQ/src/main/java/com/wxd/receiver/HelloReceiver2.java
@@ -0,0 +1,16 @@
+package com.wxd.receiver;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener(queues = "q_hello")
+public class HelloReceiver2 {
+
+    @RabbitHandler
+    public void process(String hello) {
+        System.out.println("Receiver2  : " + hello);
+    }
+
+}
